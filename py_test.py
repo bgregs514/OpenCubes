ff99a4cbabb479401d9188f44c6def14721f39ed
Ben Gregory <TBD>
2020-07-28 15:08:01 -0500

    cleaned up .py file

--- py_test.py
+++ py_test.py
@@ -1,9 +1,6 @@
 import requests
 import json
 
-# sample URL for reference
-#URL = "http://192.168.0.17:8880/api/v1/" + param + "?$select=Name";
-
 def get_config():
 	configs = {};
 	config = open("cubes.conf");
@@ -27,16 +24,4 @@ def get_names(param):
 	for item in d:
 		name_list.append(item["Name"]);
 		#print(item);
-	return name_list;
-#get_cubes("Cubes");
-
-def get_proc():
-	URL = "http://192.168.0.17:8880/api/v1/Processes?$select=Name";
-
-	response = requests.get(url = URL, auth=("ben", ""));
-	data_json = response.json();
-	d = data_json["value"];
-	name_list = [];
-	for proc in d:
-		name_list.append(proc["Name"]);
 	return name_list;
